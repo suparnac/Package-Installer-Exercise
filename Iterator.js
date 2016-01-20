df0a3b2c03292f93453463755b9c70ed2e258190
suparnac <suparnachafekar@gmail.com>
2016-01-19 21:13:51 -0600

    Update Iterator.js
    Updated the functions in Iterator

--- Iterator.js
+++ Iterator.js
@@ -4,22 +4,12 @@ var Iterator = function(items) {
 };
  
 Iterator.prototype = {
-    first: function() {
-        this.reset();
-        return this.next();
-    },
+    
     next: function() {
         return this.items[this.index++];
     },
     hasNext: function() {
-        return this.index <= this.items.length;
-    },
-    reset: function() {
-        this.index = 0;
-    },
-    each: function(callback) {
-        for (var item = this.first(); this.hasNext(); item = this.next()) {
-            callback(item);
-        }
+        return this.index < this.items.length;
     }
-}
+   
+};
